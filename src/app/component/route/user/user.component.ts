1bf23a1689cfd54a16fc843dc4a4212e0fa48bd8
Kyle Van Berendonck <kvanberendonck@gmail.com>
2017-08-05 01:50:01 +1000

    Made layout prettier

--- src/app/component/route/user/user.component.ts
+++ src/app/component/route/user/user.component.ts
@@ -13,7 +13,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
   private user: User;
