6111b3ae726d51df86e6207515cb5a936957aa02
Kyle Van Berendonck <kvanberendonck@gmail.com>
2017-08-25 22:05:19 +1000

    Switch to api2

--- src/app/interceptor/pss-api/pss-api.interceptor.ts
+++ src/app/interceptor/pss-api/pss-api.interceptor.ts
@@ -1,7 +1,7 @@
 import { XHRBackend, Request, XHRConnection, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
-const API_BASE_URL = 'https://api.pixelstarships.com/';
+const API_BASE_URL = 'https://api2.pixelstarships.com/';
 const API_USER_AGENT = 'Pixel%20Starships/56 CFNetwork/811.5.4 Darwin/16.6.0 (x86_64)';
 
 export class PixelStarshipsAPIInterceptorBackend extends XHRBackend {
